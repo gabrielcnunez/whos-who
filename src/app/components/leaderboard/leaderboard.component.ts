9de2bde9dd17ec0bfab3dfbe949eeb2f4c2bdf0a
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-09-17 16:19:03 -0700

    Flesh out leaderboard html, add ts functionality

--- src/app/components/leaderboard/leaderboard.component.ts
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -1,1 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
 
+interface Player {
+  name: string;
+  score: number;
+}
+
+@Component({
+  selector: 'app-leaderboard',
+  templateUrl: './leaderboard.component.html',
+  styleUrls: ['./leaderboard.component.css']
+})
+export class LeaderboardComponent implements OnInit {
+  @Input() title!: string;
+  @Input() maxEntries!: number;
+  @Input() showSeeMore: boolean = false;
+
+  allPlayers: Player[] = [
+    { name: '', score: 3000 },
+    { name: '', score: 2900 },
+    { name: '', score: 2800 },
+    { name: '', score: 2700 },
+    { name: '', score: 2600 },
+    { name: '', score: 2500 },
+    { name: '', score: 2400 },
+    { name: '', score: 2300 },
+    { name: '', score: 2200 },
+    { name: '', score: 2100 },
+    { name: '', score: 2000 }
+  ];
+
+  displayedPlayers: Player[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.displayedPlayers = this.allPlayers.slice(0, this.maxEntries);
+  }
+
+  showMorePlayers(): void {
+    this.displayedPlayers = this.allPlayers;
+  }
+
+}
