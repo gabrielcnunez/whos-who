d114cfee27a2110ed01681374e1917b46a4cb3a6
Sylrelessa <briana.oneal823@gmail.com>
2024-09-18 15:48:58 -0400

    put elements on gameplay page

--- src/app/gameplay/gameplay.component.ts
+++ src/app/gameplay/gameplay.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { faPlayCircle, faPauseCircle  } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-gameplay',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gameplay.component.css']
 })
 export class GameplayComponent implements OnInit {
+
+  faPlayCircle = faPlayCircle;
+  faPauseCircle = faPauseCircle;
+
+  @Input() score = 0;
+  @Input() round = 0;
+  @Input() artist1 = 'Yaosobi'
+  @Input() artist2 = 'Taylor Swift'
+  @Input() artist3 = 'King Gizzard and the Wizard Lizard'
+  @Input() artist4 = 'Three Days Grace'
 
   constructor() { }
 
