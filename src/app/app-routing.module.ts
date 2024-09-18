e660dc4eca5fcd33807312bf85bfe6b779211f5b
Sylrelessa <briana.oneal823@gmail.com>
2024-09-18 11:29:51 -0400

    created gameplay page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EndgameComponent } from './endgame/endgame.component';
+import { GameplayComponent } from './gameplay/gameplay.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'endgame', component: EndgameComponent}
+  {path: 'endgame', component: EndgameComponent},
+  {path: 'gameplay', component: GameplayComponent}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,12 @@ import { NavigationComponent } from './components/navigation/navigation.componen
 import { EndgameComponent } from './endgame/endgame.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
+import { GameplayComponent } from './gameplay/gameplay.component';
 
 const routes: Routes = [{ path: "", component: HomeComponent }];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavigationComponent, EndgameComponent],
+  declarations: [AppComponent, HomeComponent, NavigationComponent, EndgameComponent, GameplayComponent],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes), FontAwesomeModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
