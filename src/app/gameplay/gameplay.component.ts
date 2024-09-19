608600c8f41baea3de9dab297876701c40670a24
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-09-18 20:30:47 -0700

    Add howler to play song previews

--- src/app/gameplay/gameplay.component.ts
+++ src/app/gameplay/gameplay.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { faPlayCircle, faPauseCircle  } from '@fortawesome/free-solid-svg-icons';
+import { Howl} from 'howler';
 
 @Component({
   selector: 'app-gameplay',
@@ -7,6 +8,8 @@ import { faPlayCircle, faPauseCircle  } from '@fortawesome/free-solid-svg-icons'
   styleUrls: ['./gameplay.component.css']
 })
 export class GameplayComponent implements OnInit {
+  songUrl: string = '';
+  sound!: Howl;
 
   faPlayCircle = faPlayCircle;
   faPauseCircle = faPauseCircle;
@@ -19,8 +22,26 @@ export class GameplayComponent implements OnInit {
   @Input() artist4 = 'Three Days Grace'
 
   constructor() { }
-
+  
   ngOnInit(): void {
+    this.songUrl = 'https://p.scdn.co/mp3-preview/1de566ed732e7c2762c9c4432eb1ac3f6fa41d39?cid=06a826fc18d14c909746467bc9070b97'
+
+    this.sound = new Howl({
+      src: [this.songUrl],
+      html5: true
+    })
+  }
+
+  playSong() {
+    this.sound.play();
+  }
+
+  pauseSong() {
+    this.sound.pause();
+  }
+
+  stopSong() {
+    this.sound.stop();
   }
 
 }
