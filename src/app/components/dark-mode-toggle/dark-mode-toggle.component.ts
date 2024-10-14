86eac62393a0f2eb53598c0d63f947ee51c33420
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-10-14 13:25:35 -0700

    Generate dark-mode-toggle component

---
+++ src/app/components/dark-mode-toggle/dark-mode-toggle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dark-mode-toggle',
+  templateUrl: './dark-mode-toggle.component.html',
+  styleUrls: ['./dark-mode-toggle.component.css']
+})
+export class DarkModeToggleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
