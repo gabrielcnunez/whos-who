3335d40a6509515ec62736eebbd188e9a6c35a08
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-10-15 11:00:58 -0700

    Inject ThemeService and Subscription to observe dark mode value, change icon colors

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { faHome, faWrench, faTrophy } from '@fortawesome/free-solid-svg-icons';
 import { filter } from 'rxjs/operators';
+import { ThemeService } from "src/services/theme.service";
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, OnDestroy {
   isHomePage: boolean = false
+  isDarkMode: boolean = false
+  darkModeSubscription: Subscription | undefined
 
   faHome = faHome;
   faWrench = faWrench;
   faTrophy = faTrophy;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private themeService: ThemeService) { }
 
   ngOnInit(): void {
     this.router.events.pipe(
@@ -23,6 +27,16 @@ export class NavigationComponent implements OnInit {
     ).subscribe((event: any) => {
       this.isHomePage = event.url === '/';
     });
+
+    this.darkModeSubscription = this.themeService.isDarkMode$.subscribe((isDark: boolean) => {
+      this.isDarkMode = isDark;
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.darkModeSubscription) {
+      this.darkModeSubscription.unsubscribe();
+    }
   }
 
 }
