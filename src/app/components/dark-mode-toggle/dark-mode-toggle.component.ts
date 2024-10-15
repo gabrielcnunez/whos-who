0f68fc8666a8f7060590957c5f54472945801023
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-10-15 10:58:55 -0700

    Refactor dark mode toggle to use updated theme service

--- src/app/components/dark-mode-toggle/dark-mode-toggle.component.ts
+++ src/app/components/dark-mode-toggle/dark-mode-toggle.component.ts
@@ -1,28 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
 import { faSun } from "@fortawesome/free-regular-svg-icons";
 import { ThemeService } from "src/services/theme.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-dark-mode-toggle',
   templateUrl: './dark-mode-toggle.component.html',
   styleUrls: ['./dark-mode-toggle.component.css']
 })
-export class DarkModeToggleComponent implements OnInit {
-  isDarkMode: boolean
-
+export class DarkModeToggleComponent implements OnInit, OnDestroy {
+  isDarkMode: boolean = false
   faSun = faSun
   faMoon = faMoon
+  darkModeSubscription: Subscription | undefined
 
-  constructor(private themeService: ThemeService) {
-    this.isDarkMode = this.themeService.isDarkMode()
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.darkModeSubscription = this.themeService.isDarkMode$.subscribe(isDarkMode => {
+      this.isDarkMode = isDarkMode;
+    });
   }
 
   toggleTheme() {
-    this.isDarkMode = !this.isDarkMode
-    this.themeService.setDarkMode(this.isDarkMode)
+    this.themeService.setDarkMode(!this.isDarkMode)
   }
 
-  ngOnInit(): void {}
-
+  ngOnDestroy(): void {
+    if(this.darkModeSubscription) {
+      this.darkModeSubscription.unsubscribe()
+    }
+  }
 }
